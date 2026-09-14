21ca8be6889f562c042e9c3e4f4e8ee37fb12976
narasingapranav <pranavch74934@gmail.com>
2026-09-14 16:01:25 +0530

    LC minimum-difference-between-highest-and-lowest-of-k-scores: Sorting

--- LeetCode/minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ LeetCode/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -1,6 +1,8 @@
 from itertools import combinations
 class Solution:
     def minimumDifference(self, nums: List[int], k: int) -> int:
+        if len(nums)==1:
+            return 0
         nums.sort()
         a=float('inf')
         for i in range(len(nums)-k+1):
