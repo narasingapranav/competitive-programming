2132e6bd11b3d19b6cf780d3b37e9528d0d30ade
narasingapranav <pranavch74934@gmail.com>
2026-09-14 15:53:17 +0530

    LC balanced-binary-tree: Two pointers

--- LeetCode/balanced-binary-tree.py
+++ LeetCode/balanced-binary-tree.py
@@ -5,15 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def hei(self,root):
+            if not root:
+                return 0
+            return 1+max(self.hei(root.left),self.hei(root.right))
     def isBalanced(self, root: Optional[TreeNode]) -> bool:
         if not root:
             return True
-        def hei(root):
-            if not root:
-                return 0
-            return 1+max(hei(root.left),hei(root.right))
-        lh=hei(root.left)
-        rh=hei(root.right)
+        lh=self.hei(root.left)
+        rh=self.hei(root.right)
         if abs(lh-rh)<=1 and self.isBalanced(root.left) and self.isBalanced(root.right):
             return True
         return False
