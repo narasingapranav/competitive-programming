03c5acd1be4708e2c0df9f8e343e6c5528a025d9
narasingapranav <pranavch74934@gmail.com>
2026-09-14 14:59:23 +0530

    LC sequential-digits: Direct simulation / brute force

--- LeetCode/sequential-digits.py
+++ LeetCode/sequential-digits.py
@@ -1,12 +1,15 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        res=[]
-        s="123456789"
-        l=str(low)
-        h=str(high)
-        for i in range(len(l),len(h)+1):
-            for j in range(10-i):
-                n=int(s[j:j+i])
-                if low<=n<=high:
-                    res.append(n)
-        return res
+        ans = []
+
+        s = "123456789"
+        l = str(low)
+        h = str(high)
+
+        for length in range(len(l), len(h) + 1):
+            for start in range(0, 10 - length):
+                num = int(s[start:start + length])
+                if low <= num <= high:
+                    ans.append(num)
+
+        return ans
