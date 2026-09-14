6a6ae280d5288722ba9c7edd8480ee1fa82f464c
narasingapranav <pranavch74934@gmail.com>
2026-09-14 15:30:15 +0530

    LC minimum-initial-energy-to-finish-tasks: Sorting

--- LeetCode/minimum-initial-energy-to-finish-tasks.py
+++ LeetCode/minimum-initial-energy-to-finish-tasks.py
@@ -1,6 +1,6 @@
 class Solution:
     def minimumEffort(self, tasks: List[List[int]]) -> int:
-        tasks.sort(key=lambda t: t[0] - t[1])
+        tasks.sort(key=lambda t: -(t[1] - t[0]))
         curr = 0
         ans  = 0
         for actual, minimum in tasks:
